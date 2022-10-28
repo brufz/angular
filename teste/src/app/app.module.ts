36b345a7f36dd44f97004992f7a4e5648db8226e
Bruna Franchi <bfzdev@gmai.com>
2022-10-28 12:04:00 -0300

    inicio task week01

--- teste/src/app/app.module.ts
+++ teste/src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DishdetailsComponent } from './dishdetails/dishdetails.component';
+import { MenuComponent } from './menu/menu.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailsComponent
   ],
   imports: [
     BrowserModule,
