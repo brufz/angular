32e5c8296452c10567a889207ed0401b85b5c2b9
Bruna Franchi <bfzdev@gmai.com>
2022-10-28 14:54:05 -0300

    task 2 e 3

--- teste/src/app/app.module.ts
+++ teste/src/app/app.module.ts
@@ -14,6 +14,7 @@ import 'hammerjs';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { MatButtonModule } from '@angular/material/button';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
