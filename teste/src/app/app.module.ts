a59fa7bb49a3408c5358a16d7e217c9ecbdcf0d0
Bruna Franchi <bfzdev@gmai.com>
2022-11-04 10:24:22 -0300

    dialog

--- teste/src/app/app.module.ts
+++ teste/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
-
 
 import { AppComponent } from './app.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
@@ -26,6 +26,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -47,13 +49,18 @@ import { LeaderService } from './services/leader.service';
     MatCardModule,
     MatButtonModule,
     MatListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
   ],
+  entryComponents: [
+    LoginComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
