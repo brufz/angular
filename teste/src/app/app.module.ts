703829f7c8b9ceb1327dc7e138a180c7aec3d15f
Bruna Franchi <bfzdev@gmai.com>
2022-11-07 16:30:39 -0300

    reactive forms validators

--- teste/src/app/app.module.ts
+++ teste/src/app/app.module.ts
@@ -10,9 +10,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -50,6 +53,7 @@ import { LoginComponent } from './login/login.component';
     MatToolbarModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
@@ -58,7 +62,9 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatCheckboxModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
---
+++ teste/src/app/shared/feedback.ts
@@ -0,0 +1,13 @@
+export class Feedback {
+    firstname : string;
+    lastname : string;
+    telnum : number;
+    email : string;
+    agree : boolean;
+    contactType: string;
+    message : string;
+}
+
+export const ContactType = [
+'None', 'Tel', 'Email'
+]
