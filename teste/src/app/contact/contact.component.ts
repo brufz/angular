45d96ee927e4c25bf2700accdfb0a2bb6032b128
Bruna Franchi <bfzdev@gmai.com>
2022-11-09 15:18:43 -0300

    validators reactive form

--- teste/src/app/contact/contact.component.ts
+++ teste/src/app/contact/contact.component.ts
@@ -21,21 +21,80 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  createForm(){
-    this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+  formErrors = {
+    'firstname' : '',
+    'lastname' : '',
+    'telnum' : '',
+    'email' : '' 
+   }
+
+   validationMessages = {
+    'firstname' : {
+      'required':'First name is required',
+      'minlength':'First name must be at least 2 characteres',
+      'maxlength':'First name cannot be more then 25 characteres'
+    },
+    'lastname':{
+      'required':'Last name is required',
+      'minlength':'Last name must be at least 2 characteres',
+      'maxlength':'Last name cannot be more then 25 characteres'
+    },
+    'telnum':{
+      'required':'Tel. Number is required',
+      'pattern':'Tel. number must contain just numbers'
+    },
+    'email':{
+      'required':'Email is required',
+      'email':'Email must be valid'
+    }
+   }
+
+  createForm() : void{
+    this.feedbackForm = this.fb.group({ //formGroup = FormBuilder.group
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contactType: 'None',
       message: ''
 
+      
+
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged(); // (re)set validation messages now
+  }
+
+  
+  
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+
+        }
+      }
+    }
   }
 
   onSubmit(){
-    this.feedback = this.feedbackForm.value;
+    this.feedback = this.feedbackForm.value; //Feedback = FormGroup.value
     console.log(this.feedback);
     this.feedbackForm.reset({
       firstname: '',
